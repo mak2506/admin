f28f7b901a816d3838369e0ef4ed3c576f4ad35f
Mansi Kumari <73823914+mak2506@users.noreply.github.com>
2022-03-27 21:47:33 +0530

    delete

--- Admin.java
+++
@@ -1,7 +0,0 @@
-HttpSession hs=request.getSession(false);//this will return session reference
-		if(hs!=null)
-		{
-			hs.removeAttribute("admin");
-			hs.invalidate();
-			response.sendRedirect("/buko/Admin");
-		}
