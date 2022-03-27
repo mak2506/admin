0e0dde4089187f919830689c571326b731e72301
Mansi Kumari <73823914+mak2506@users.noreply.github.com>
2022-03-27 22:03:11 +0530

    done

--- uploader.java
+++ uploader.java
@@ -1,48 +1,16 @@
-//String id=request.getParameter("id");
-		String name=request.getParameter("name");
-		String password=request.getParameter("password");		
-		String email=request.getParameter("email");
-		String gender=request.getParameter("gender");
-		String phone=request.getParameter("phone");
-		System.out.println(name);
-		System.out.println(password);
-		System.out.println(email);
-		System.out.println(gender);
-		System.out.println(phone);
+// TODO Auto-generated method stub
+		Part part=request.getPart("file");
+		String filename=part.getSubmittedFileName();
+		//System.out.print(filename);
 		
-		Connection con=DBConnection.createConnection();
-		String strinsert="insert into uploader (name, password, email, gender, phonenumber, date) values (?,?,?,?,?,?)";
-		 java.util.Date d=new java.util.Date();
-		 java.sql.Date sql_date=new java.sql.Date(d.getTime());//fr system print date
-		 PreparedStatement ps=null;
-		 try {	
-			 ps=con.prepareStatement(strinsert);
-			 //ps.setString(1,id);
-			 ps.setString(1,name);
-			 ps.setString(2,password);
-			 ps.setString(3,email);
-			 ps.setString(4,gender);
-			 ps.setString(5,phone);
-			 ps.setDate(6, sql_date);
-			 System.out.println(ps);
-			 int status=ps.executeUpdate();//error
-			 if(status>0)
-				 	System.out.println("uploader login sucessfully done");			 
-		 	}
-		 	catch(SQLException se)
-		 	{
-			 se.printStackTrace();
-		 	}
-		 	finally {
-			 try {
-				 if(ps!=null)
-					 ps.close();
-				 
-			 }
-			 catch(SQLException se)
-			 {
-				 se.printStackTrace();
-				 
-			 }
-			 
-		 }
+		InputStream is=part.getInputStream();
+		byte []data = new byte[is.available()];
+		is.read(data);
+		String path= "C:/Users/pc/Desktop/BookStore/buko/src/main/webapp/images/"+filename;
+		//System.out.println(path);
+		FileOutputStream fos = new FileOutputStream(path);
+		fos.write(data);
+		fos.close();
+		PrintWriter pw=response.getWriter();
+		response.setContentType("text/html");
+		pw.println("<img src="+path+"'>");
