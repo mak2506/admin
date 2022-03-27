60567350873d7b4514b2ea89ca803ddbd3b41c3e
Mansi Kumari <73823914+mak2506@users.noreply.github.com>
2022-03-27 22:00:15 +0530

    done

--- uploader.java
+++ uploader.java
@@ -1,55 +1,48 @@
-String email=request.getParameter("email");//user id is control name text box
-		String pass=request.getParameter("password");
-		//System.out.print(email);
-		//System.out.print(pass);
+//String id=request.getParameter("id");
+		String name=request.getParameter("name");
+		String password=request.getParameter("password");		
+		String email=request.getParameter("email");
+		String gender=request.getParameter("gender");
+		String phone=request.getParameter("phone");
+		System.out.println(name);
+		System.out.println(password);
+		System.out.println(email);
+		System.out.println(gender);
+		System.out.println(phone);
 		
 		Connection con=DBConnection.createConnection();
-		String strsql="select * from uploader where email=? and password=?";
-		PreparedStatement ps=null;
-		ResultSet rs=null;
-		try {
-			ps=con.prepareStatement(strsql);
-			ps.setString(1, email);
-			ps.setString(2, pass);
-			//System.out.print(ps);
-			 rs=ps.executeQuery();
-			if(rs.next())
-			{
-				//System.out.println("Hello");
-				HttpSession hs=request.getSession();//this will create a session
-				hs.setAttribute("userkey", email);
-				System.out.println("Unique session id is: "+hs.getId());//getId will return the token or session id
-				response.sendRedirect("/buko/uploader/uploaderhome.jsp");
-			}
-			else
-			{
-				//System.out.println(Message.LOGIN_ERROR_MESSAGE);
-				request.setAttribute("message", Message.LOGIN_ERROR_MESSAGE);
-				RequestDispatcher rd=request.getRequestDispatcher("/uploader/uploaderlogin.jsp");
-				rd.forward(request, response);
-				//response.sendRedirect("/buko/uploader/uploaderlogin.jsp");
-				
-			}
-		}
-		catch(SQLException se)
-		{
-			se.printStackTrace();
-		}
-	
-	finally {
-		if(ps!=null)
-			try {
-				ps.close();
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		if(rs!=null)
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-	}	
+		String strinsert="insert into uploader (name, password, email, gender, phonenumber, date) values (?,?,?,?,?,?)";
+		 java.util.Date d=new java.util.Date();
+		 java.sql.Date sql_date=new java.sql.Date(d.getTime());//fr system print date
+		 PreparedStatement ps=null;
+		 try {	
+			 ps=con.prepareStatement(strinsert);
+			 //ps.setString(1,id);
+			 ps.setString(1,name);
+			 ps.setString(2,password);
+			 ps.setString(3,email);
+			 ps.setString(4,gender);
+			 ps.setString(5,phone);
+			 ps.setDate(6, sql_date);
+			 System.out.println(ps);
+			 int status=ps.executeUpdate();//error
+			 if(status>0)
+				 	System.out.println("uploader login sucessfully done");			 
+		 	}
+		 	catch(SQLException se)
+		 	{
+			 se.printStackTrace();
+		 	}
+		 	finally {
+			 try {
+				 if(ps!=null)
+					 ps.close();
+				 
+			 }
+			 catch(SQLException se)
+			 {
+				 se.printStackTrace();
+				 
+			 }
+			 
+		 }
